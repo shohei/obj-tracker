b5bd84e02a7247f3d4aaa53547b41f7e70e9e9de
Shohei Aoki <shoaok@gmail.com>
2016-07-14 11:28:00 +0900

    update result

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
-// #include <opencv2/core/core.hpp>
-// #include <opencv2/imgproc.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <opencv2/imgproc/imgproc.hpp>
-// #include <iostream>
-// // #include <opencv2/imgcodecs.hpp>
-// #include <opencv2/video.hpp>
-// #include <opencv2/imgproc.hpp>
-// #include <climits>
-// #include <cfloat>
-// #include <vector>
-// #include <limits>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/imgproc.hpp>
-// #include <opencv2/video/tracking.hpp>
 #include <iostream>
 
-#pragma comment(lib, "opencv_core310.lib")
-#pragma comment(lib, "opencv_imgcodecs310.lib")
-#pragma comment(lib, "opencv_videoio310.lib")
-#pragma comment(lib, "opencv_highgui310.lib")
-#pragma comment(lib, "opencv_tracking310.lib")
-#pragma comment(lib, "opencv_imgproc310.lib")
+// #pragma comment(lib, "opencv_core310.lib")
+// #pragma comment(lib, "opencv_imgcodecs310.lib")
+// #pragma comment(lib, "opencv_videoio310.lib")
+// #pragma comment(lib, "opencv_highgui310.lib")
+// #pragma comment(lib, "opencv_tracking310.lib")
+// #pragma comment(lib, "opencv_imgproc310.lib")
 
 using namespace cv;
 using namespace std;
 
-// Rect2d selectROI(const String &video_name, const Mat &frame);
 
 int main(int argc, char* argv[])
 {
@@ -75,6 +61,8 @@ int main(int argc, char* argv[])
    Ptr<cv::Tracker> tracker = Tracker::create("KCF");
    tracker->init(frame, roi);
 
+   // cv::VideoWriter writer( "output.avi", cv::VideoWriter::fourcc('M','J','P','G'), cap.get(cv::CAP_PROP_FPS), cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT)));
+   cv::VideoWriter writer( "output.mov", cv::VideoWriter::fourcc('M','P','4','V'), cap.get(cv::CAP_PROP_FPS), cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT)));
 
   while (1)
   {
@@ -98,6 +86,8 @@ int main(int argc, char* argv[])
 
     imshow("MyVideo", frame); //show the frame in "MyVideo" window
 
+    writer << frame;
+
     if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
     {
       cout << "esc key is pressed by user" << endl;
@@ -107,57 +97,3 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-
-// Rect2d selectROI(const String &video_name, const Mat &frame)
-// {
-//     struct Data
-//     {
-//         Point center;
-//         Rect2d box;
-//
-//         static void mouseHandler(int event, int x, int y, int flags, void *param)
-//         {
-//             Data *data = (Data*)param;
-//             switch( event )
-//             {
-//             // start to select the bounding box
-//             case EVENT_LBUTTONDOWN:
-//                 data->box = cvRect( x, y, 0, 0 );
-//                 data->center = Point2f((float)x,(float)y);
-//                 break;
-//             // update the selected bounding box
-//             case EVENT_MOUSEMOVE:
-//                 if(flags == 1)
-//                 {
-//                     data->box.width  = 2 * (x - data->center.x);
-//                     data->box.height = 2 * (y - data->center.y);
-//                     data->box.x = data->center.x - data->box.width / 2.0;
-//                     data->box.y = data->center.y - data->box.height / 2.0;
-//                 }
-//                 break;
-//             // cleaning up the selected bounding box
-//             case EVENT_LBUTTONUP:
-//                 if( data->box.width < 0 )
-//                 {
-//                     data->box.x += data->box.width;
-//                     data->box.width *= -1;
-//                 }
-//                 if( data->box.height < 0 )
-//                 {
-//                     data->box.y += data->box.height;
-//                     data->box.height *= -1;
-//                 }
-//                 break;
-//             }
-//         }
-//     } data;
-//
-//     setMouseCallback(video_name, Data::mouseHandler, &data);
-//     while(waitKey(1) < 0)
-//     {
-//         Mat draw = frame.clone();
-//         rectangle(draw, data.box, Scalar(255,0,0), 2, 1);
-//         imshow(video_name, draw);
-//     }
-//     return data.box;
-// }
